c5a682723d5254e824f818509983e24f3cb07d2c
Andrew Hartnell <thirdbytetaken@gmail.com>
2021-12-11 09:38:50 -0800

    Require Stronger Sender option for responses added

--- Assets/Scripts/AIManager.cs
+++ Assets/Scripts/AIManager.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class AIManager : MonoBehaviour
 {
+    // Determines the receivers response to actions
     public static Response BestResponse(Event ce, Country sender)
     {
         List<Response> PossibleResponses = new List<Response>();
         foreach (Response Response in ce.action.Responses)
         {
             var Relation = sender.Relations[ce.receiver.ID].Value;
+
             if (Relation <= Response.MinRelation || Relation >= Response.MaxRelation) continue;
             if (ce.receiver.Money < Response.MinMoney) continue; // If the person being asked for a loan doesn't have enough money
-            if (ce.receiver.WarPower < Response.MinWarPower) continue; // If the person being asked for a loan doesn't have enough money
+            if (ce.receiver.WarPower < Response.MinWarPower) continue;
+            if (Response.RequireStrongerSender && ce.sender.WarPower < ce.receiver.WarPower) continue;
+
             PossibleResponses.Add(Response);
         }
         return DevTools.RandomListValue(PossibleResponses);
