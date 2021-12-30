070510a3e27d934ffc9383393a522e94499def4d
Jakob Kuehnen <jakobqnen@gmail.com>
2021-12-30 01:15:59 -0600

    Change to hex approach

--- Assets/test.cs
+++ Assets/test.cs
@@ -5,33 +5,23 @@ using UnityEngine;
 public class test : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject originalImage;
+    public Canvas canvas;
+    public int depth;
     void Start()
     {
-        int vertexCount = Random.Range(3, 6);
-        Mesh mesh = new Mesh();
-        Vector3[] vertices = new Vector3[vertexCount];
-        Vector2[] uvs = new Vector2[vertexCount];
-        int[] triangles = new int[vertexCount];
-
-
-        Vector3[] PossVertices = new Vector3[vertexCount];
-        for (int i = 0; i < PossVertices.Length; i++)
+        SpriteRenderer spriteRenderer = originalImage.GetComponent<SpriteRenderer>();
+        Vector3 rendererSize = spriteRenderer.bounds.size / canvas.GetComponent<RectTransform>().localScale.x;
+        GameObject newImage;
+        for (int i = 0; i < depth; i++)
         {
-            PossVertices[i] = new Vector3(Random.Range(-100, 101), Random.Range(-100, 101));
-            vertices[i] = PossVertices[i];
-            uvs[i] = new Vector2((PossVertices[i].x + 100) / 200f, (PossVertices[i].y + 100) / 200f);
-            triangles[i] = i;
+            newImage = GameObject.Instantiate(originalImage, originalImage.transform.parent);
+            //print(spriteRenderer.sprite.rect.width + " " + spriteRenderer.sprite.rect.height);
+            //print(spriteRenderer.bounds.size.x + " " + spriteRenderer.bounds.size.y);
+            //print(canvas.GetComponent<RectTransform>().localScale.x + " " + canvas.GetComponent<RectTransform>().localScale.y);
+            newImage.transform.localPosition = new Vector3(rendererSize.x * -.5f * (i + 1), rendererSize.y * .75f * (i + 1));
         }
-        mesh.vertices = vertices;
-        mesh.uv = uvs;
-        mesh.triangles = triangles;
 
-        GetComponent<MeshFilter>().mesh = mesh;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
 
     }
 }
