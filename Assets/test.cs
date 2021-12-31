331700f8df31fa35719f6be24153b1585a4804da
Jakob Kuehnen <jakobqnen@gmail.com>
2021-12-31 15:10:11 -0600

    Add centering and zooming for hexes
    Switch from spriterenderer to image components

--- Assets/test.cs
+++ Assets/test.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UI;
 
 public class test : MonoBehaviour
 {
@@ -18,13 +19,13 @@ public class test : MonoBehaviour
         new Vector3(-.5f, -.75f), // Down and Left
         new Vector3(-1f, 0f)  // Left
     };
-    SpriteRenderer spriteRenderer;
-    Vector3 rendererSize;
+    public float ScrollSpeed;
+    Image image;
+    Vector3 imageSize;
     void Start()
     {
-        spriteRenderer = originalImage.GetComponent<SpriteRenderer>();
-        rendererSize = spriteRenderer.bounds.size / canvas.GetComponent<RectTransform>().localScale.x;
-        print(new Vector3(0.00f, 0.00f, 0.00f) == new Vector3(0.00f, 0.00f, 0.00f));
+        image = originalImage.GetComponent<Image>();
+        imageSize = new Vector3(image.sprite.rect.width, image.sprite.rect.height);
     }
     public void Generate()
     {
@@ -38,29 +39,31 @@ public class test : MonoBehaviour
         {
             GameObject newImage = GameObject.Instantiate(currentImage, currentImage.transform.position, new Quaternion(), currentImage.transform.parent);
 
-
             List<Vector3> possibleDirections = new List<Vector3>();
-            //print("**************occupied poses******************");
-            //            occupiedPositions.ForEach(x => print(x));
             foreach (Vector3 offset in hexoffsets)
             {
-                //print("potential pos: " + ());
-                //print("open?: " + !occupiedPositions.Any(x => x == newImage.transform.localPosition + new Vector3(Mathf.Round(rendererSize.x * offset.x), Mathf.Round(rendererSize.y * offset.y))));
-                Vector3 offsetPosition = DevTools.RoundVector3(newImage.transform.localPosition + new Vector3(rendererSize.x * offset.x, rendererSize.y * offset.y), 1);
+                Vector3 offsetPosition = DevTools.RoundVector3(newImage.transform.localPosition + new Vector3(imageSize.x * offset.x, imageSize.y * offset.y), 1);
                 if (!occupiedPositions.Contains(offsetPosition)) // If the position in this direction is unoccupied                
                     possibleDirections.Add(offset);
-
             }
-
             int newDirection = Random.Range(0, possibleDirections.Count);
             Vector3 newOffset = possibleDirections[newDirection];
 
-            newImage.transform.localPosition += new Vector3(rendererSize.x * newOffset.x, rendererSize.y * newOffset.y);
+            newImage.transform.localPosition += new Vector3(imageSize.x * newOffset.x, imageSize.y * newOffset.y);
             occupiedPositions.Add(DevTools.RoundVector3(newImage.transform.localPosition, 1));
             currentImage = newImage;
         }
-        //print("**************");
-        //occupiedPositions.ForEach(x => print(x));
+        Vector3 v3_mostLeftHex = occupiedPositions.OrderBy(x => x.x).First();
+        Vector3 v3_mostRightHex = occupiedPositions.OrderBy(x => x.x).Last();
+        Vector3 v3_mostDownHex = occupiedPositions.OrderBy(x => x.y).First();
+        Vector3 v3_mostUpHex = occupiedPositions.OrderBy(x => x.y).Last();
+        Vector3 center = new Vector3(occupiedPositions.Average(x => x.x), occupiedPositions.Average(x => x.y));
+        originalImage.transform.parent.localPosition = -center;
 
     }
+    void Update()
+    {
+        originalImage.transform.parent.localScale += new Vector3(1, 1) * ScrollSpeed * Input.GetAxis("Mouse ScrollWheel");
+    }
+
 }
