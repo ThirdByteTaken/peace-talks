756960addfeebcade4a6b202d51ac8d043ca34f8
Jakob Kuehnen <jakobqnen@gmail.com>
2021-12-30 19:59:12 -0600

    Add random hex shape generation

--- Assets/test.cs
+++ Assets/test.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class test : MonoBehaviour
 {
@@ -8,20 +9,58 @@ public class test : MonoBehaviour
     public GameObject originalImage;
     public Canvas canvas;
     public int depth;
+    private static Vector3[] hexoffsets = new Vector3[]
+    {
+        new Vector3(-.5f, .75f), // Up and Left
+        new Vector3(.5f, .75f), // Up and Right
+        new Vector3(1f, 0f), // Right
+        new Vector3(.5f, -.75f), // Down and Right
+        new Vector3(-.5f, -.75f), // Down and Left
+        new Vector3(-1f, 0f)  // Left
+    };
+    SpriteRenderer spriteRenderer;
+    Vector3 rendererSize;
     void Start()
     {
-        SpriteRenderer spriteRenderer = originalImage.GetComponent<SpriteRenderer>();
-        Vector3 rendererSize = spriteRenderer.bounds.size / canvas.GetComponent<RectTransform>().localScale.x;
-        GameObject newImage;
+        spriteRenderer = originalImage.GetComponent<SpriteRenderer>();
+        rendererSize = spriteRenderer.bounds.size / canvas.GetComponent<RectTransform>().localScale.x;
+        print(new Vector3(0.00f, 0.00f, 0.00f) == new Vector3(0.00f, 0.00f, 0.00f));
+    }
+    public void Generate()
+    {
+        for (int i = originalImage.transform.parent.childCount - 1; i > 0; i--)
+            GameObject.Destroy(originalImage.transform.parent.GetChild(i).gameObject);
+
+        GameObject currentImage = originalImage;
+        List<Vector3> occupiedPositions = new List<Vector3>();
+        occupiedPositions.Add(originalImage.transform.localPosition);
         for (int i = 0; i < depth; i++)
         {
-            newImage = GameObject.Instantiate(originalImage, originalImage.transform.parent);
-            //print(spriteRenderer.sprite.rect.width + " " + spriteRenderer.sprite.rect.height);
-            //print(spriteRenderer.bounds.size.x + " " + spriteRenderer.bounds.size.y);
-            //print(canvas.GetComponent<RectTransform>().localScale.x + " " + canvas.GetComponent<RectTransform>().localScale.y);
-            newImage.transform.localPosition = new Vector3(rendererSize.x * -.5f * (i + 1), rendererSize.y * .75f * (i + 1));
-        }
+            GameObject newImage = GameObject.Instantiate(currentImage, currentImage.transform.position, new Quaternion(), currentImage.transform.parent);
 
+
+            List<Vector3> possibleDirections = new List<Vector3>();
+            //print("**************occupied poses******************");
+            //            occupiedPositions.ForEach(x => print(x));
+            foreach (Vector3 offset in hexoffsets)
+            {
+                //print("potential pos: " + ());
+                //print("open?: " + !occupiedPositions.Any(x => x == newImage.transform.localPosition + new Vector3(Mathf.Round(rendererSize.x * offset.x), Mathf.Round(rendererSize.y * offset.y))));
+                Vector3 offsetPosition = DevTools.RoundVector3(newImage.transform.localPosition + new Vector3(rendererSize.x * offset.x, rendererSize.y * offset.y), 1);
+                if (!occupiedPositions.Contains(offsetPosition)) // If the position in this direction is unoccupied                
+                    possibleDirections.Add(offset);
+
+            }
+
+            int newDirection = Random.Range(0, possibleDirections.Count);
+            Vector3 newOffset = possibleDirections[newDirection];
+
+            newImage.transform.localPosition += new Vector3(rendererSize.x * newOffset.x, rendererSize.y * newOffset.y);
+            occupiedPositions.Add(DevTools.RoundVector3(newImage.transform.localPosition, 1));
+            currentImage = newImage;
+        }
+        //print("**************");
+        //occupiedPositions.ForEach(x => print(x));
 
     }
 }
